05d0e55e9b85c6d35a05b1aac9667d33e77cfd0c
phamquockhanh1312 <24100013@st.phenikaa-uni.edu.vn>
2026-09-09 12:48:59 +0700

    Update server.js

--- news/server.js
+++ news/server.js
@@ -1,1 +1,13 @@
-console.log("Hello Node.js");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/html; charset=utf-8"
+    });
+
+    res.end("<h1>Hello Node.js</h1>");
+});
+
+server.listen(3000, () => {
+    console.log("Server is running at http://localhost:3000");
+});
